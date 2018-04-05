a4f84ca9fa0722a64a40ea513eacd114afc6db44
Timothy Wickey <timothywickey@Timothys-MBP-2.lan>
2018-04-05 16:09:10 -0400

    CHANGES (in progress)
    Fix ad player and add the VAST tag

--- src/ui/popover/wasshop/wasshop.dialog.ts
+++ src/ui/popover/wasshop/wasshop.dialog.ts
@@ -30,6 +30,10 @@ export class WasShop {
   public adnotready = true;
   private loggedin = false;
 
+  private vastAdTag = 'https://ima3vpaid.appspot.com/?adTagUrl=https%3A%2F%2Fgoogleads.g.doubleclick.net%2Fpagead%2Fads%3Fclient%3Dca-video-pub-5512390705137507%26slotname%3D3326280305%2F2027455546%26ad_type%3Dvideo_text_image%26description_url%3Dhttp%253A%252F%252Fwickeyappstore.com%26max_ad_duration%3D60000%26videoad_start_delay%3D0&type=js';
+
+  private vastAdID: string;
+
   constructor(
     public dialog: MatDialog,
     private ref: ChangeDetectorRef,
@@ -63,74 +67,89 @@ export class WasShop {
       }
     });
   }
-  offerwall() {
-    this.dialog.open(WasUp, { data: { title: 'Ad watched', icon: 'card_giftcard', body: 'You got a coin!' } });
-  }
+
   // ad stuff
 
   openedFreeCoins() {
     console.log('openedFreeCoins');
     if (this.loggedin === true) {
-      this.loadAd();
+      if (this.vastAdTag) {
+        this.loadAd();
+      }
     }
   }
   closedFreeCoins() {
     console.log('closedFreeCoins');
-    this.adnotready = true;
-    this.vastplayer.stopAd();
+    if (this.vastAdTag) {
+      this.adnotready = true;
+      this.vastplayer.stopAd();
+    }
   }
-  loadAd() {
+  createVastplayer_1() {
     if (this.vastplayer === undefined) {
       console.log('create vast player');
       this.vastplayer = new (<any>window).VASTPlayer(document.getElementById('wasadcontainer'));
+      this.vastplayer.once('AdStarted', () => {
+        // console.log('Ad started', this.vastAdID);
+        this.userService.adVideoStart(this.vastAdID);
+      });
+      this.vastplayer.once('AdStopped', () => {
+        // console.log('Ad finished playback!');
+        this.userService.adVideoEnd(this.vastAdID);
+      });
     } else {
-      console.log('reuse vast player');
+      console.log('reuse vast player'); // BUT have to reset these listeners else they will not happen
+      this.vastplayer.once('AdStarted', () => {
+        // console.log('Ad STARTED', this.vastAdID);
+        this.userService.adVideoStart(this.vastAdID);
+      });
+      this.vastplayer.once('AdStopped', () => {
+        // console.log('Ad FINISHED playback!');
+        this.userService.adVideoEnd(this.vastAdID);
+      });
     }
+  }
+  loadAd() {
+    this.createVastplayer_1();
+
     this.addBtnText = 'Checking for Ad';
     this.ref.detectChanges();
-    this.vastplayer.load(
-      // 'https://ads.aerserv.com/as/?plc=1000741&cb=&ip=&make=&model=&os=&osv=&type=&ua=&url=&lat=&long=&locationsource=&age=&yob=&gender=&coppa=&vpaid=&vph=&vpw='
-      'https://ima3vpaid.appspot.com/?adTagUrl=https%3A%2F%2Fgoogleads.g.doubleclick.net%2Fpagead%2Fads%3Fclient%3Dca-video-pub-2393320645055022%26slotname%3D1989239344%2F6896802175%2F5200567630%26ad_type%3Dvideo%26description_url%3Dhttp%253A%252F%252Fwickeyappstore.com%26videoad_start_delay%3D0&type=js'
-      // 'https://net.rayjump.com/openapi/ads?app_id=100148&unit_id=30452&client_ip=188.0.175.167&sign=f3a6ebfd8fdcd87d4f982cdf1052c597&platform=1&ad_num=1&os_version=6.0.1&package_name=video.cut.editor&gaid=f7140461-c01f-431f-9c05-c52f43d46e05&android_id=6be1afe3b0f8a5e2&useragent=Mozilla%2F5.0%20(Linux%3B%20Android%206.0.1%3B%20SM-T355%20Build%2FMMB29M%3B%20wv)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Version%2F4.0%20Chrome%2F63.0.3239.111%20Safari%2F537.36&unit_size=320x480&is_vast=1&http_req=2'
-    ).then(() => {
+    this.vastplayer.load(this.vastAdTag
+    ).then((_vastobj) => {
       this.adnotready = false;
       this.addBtnText = 'Watch Ad';
       this.ref.detectChanges();
       console.log('an ad is ready');
+      // TODO: Get currently playing video ID, I don't know if this is correct or just the first ad ID.
+      this.vastAdID = _vastobj.vast.ads[0].id;
+      console.log('print out the ad');
+      console.log(_vastobj.vast.ads);
     }).catch((reason) => {
-      setTimeout(() => {
-        console.log('watchAd error: ', reason);
-        const noAdsError = /AdError 1009/gi; // g=global, i=case insensitive
-        const genericError = /Request has been terminated/gi;
+      console.log('watchAd error: ', reason);
+      const noAdsError = /AdError 1009/gi; // g=global, i=case insensitive
+      const noAds2Error = /Reduce of empty array with no initial value/gi;
+      const genericError = /Request has been terminated/gi;
 
-        const errorstring = String(reason);
-        if (errorstring.search(noAdsError) > -1) {
-          console.log('No Ad available');
-          this.addBtnText = 'No Ads. Check back later!';
-          this.ref.detectChanges();
-        } else if (errorstring.search(genericError) > -1) {
-          console.log('Request has been terminated');
-          this.addBtnText = 'Request terminated. Check later';
-          this.ref.detectChanges();
-        } else {
-          console.log('Unknown Error');
-          this.addBtnText = 'Unknown error. Please try later';
-          this.ref.detectChanges();
-        }
-      }, 0);
+      const errorstring = String(reason);
+      if (errorstring.search(noAdsError) > -1 || errorstring.search(noAds2Error) > -1) {
+        console.log('No Ad available');
+        this.addBtnText = 'No Ads. Check back later!';
+        this.ref.detectChanges();
+      } else if (errorstring.search(genericError) > -1) {
+        console.log('Request has been terminated');
+        this.addBtnText = 'Request terminated. Check later';
+        this.ref.detectChanges();
+      } else {
+        console.log('Unknown Error');
+        this.addBtnText = 'Unknown error. Please try later';
+        this.ref.detectChanges();
+      }
     });
   }
 
 
-
-  awardCoin() {
-    // wasup doesn't animate properly, so we use alert
-    // this.dialog.open(WasUp, { data: { title: 'Ad watched', icon: 'card_giftcard', body: 'You got a coin!' } });
-    this.loadAd();
-  }
   watchAd() {
     console.log('showVideoAd');
-    this.adnotready = true;
     this.addBtnText = 'Ad Playing...';
     this.vastplayer.startAd();
     this.vastplayer.once('AdStopped', () => {
@@ -138,7 +157,7 @@ export class WasShop {
       this.addBtnText = 'YOU GOT A COIN!';
       this.ref.detectChanges();
       setTimeout(() => {
-        this.awardCoin();
+        this.loadAd(); // load another ad
       }, 2500);
     });
   }
