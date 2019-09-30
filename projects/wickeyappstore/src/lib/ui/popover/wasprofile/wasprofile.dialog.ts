f3942cfeec07a2ee6590b3a241d1d2890a03c79a
wickeym <wickeym@gmail.com>
2019-09-30 10:35:15 -0400

    FIX: Use node >= 10.16

--- projects/wickeyappstore/src/lib/ui/popover/wasprofile/wasprofile.dialog.ts
+++ projects/wickeyappstore/src/lib/ui/popover/wasprofile/wasprofile.dialog.ts
@@ -24,7 +24,7 @@ export class WasProfile implements OnInit {
   /**@ignore*/
   public loggedin = false;
   /** @ignore */
-  public version = '2.19.1';
+  public version = '2.19.2';
   /**@ignore*/
   constructor(
     public userService: UserService,
